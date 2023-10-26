190361cab90dfc4f342b6063cde4e42d7bd9782d
Gazi Robiul <75046495+sagazirobiul@users.noreply.github.com>
2023-10-26 16:46:20 +0600

    set a parallelogram box

--- scripts/area-calculate.js
+++ scripts/area-calculate.js
@@ -8,3 +8,32 @@ function calculateTriangle(){
     const area = 0.5 * base * height;
     document.getElementById('triangle-area').innerText = area;
 }
+
+
+function calculateRectangle(){
+    const widthField = document.getElementById('rectangle-width');
+    const width = parseFloat(widthField.value);
+
+    const lengthField = document.getElementById('rectangle-length');
+    const length = parseFloat(lengthField.value);
+
+    const area = width * length;
+    document.getElementById('rectangle-area').innerText = area;
+}
+
+
+function calculateParallelogram(){
+    const base = getFieldValue('parallelogram-base')
+    const height = getFieldValue('parallelogram-height')
+
+    const area = base * height
+    document.getElementById('parallelogram-area').innerText = area;
+}
+
+
+function getFieldValue(fieldId){
+    const field = document.getElementById(fieldId);
+    const value = parseFloat(field.value);
+
+    return value;
+}
